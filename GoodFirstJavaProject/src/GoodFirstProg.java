e5c0a54b8a65f4fe06b92e71e3a85dd4ed7936bd
nelsona8230 <NelsonA8230@sduhsd.lan>
2017-11-29 11:48:38 -0800

    The GoodFirstProgram assignment

---
+++ GoodFirstJavaProject/src/GoodFirstProg.java
@@ -0,0 +1,15 @@
+//this program sends a list of messages to the display
+public class GoodFirstProg 
+{
+	public static void main( String[] args )
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello Again");
+		System.out.println("I like typing like this.");
+		System.out.println("This is fun.");
+		System.out.println("Yay! Printing.");
+		System.out.println("I'd much rather you 'not'.");
+		System.out.println("I \"said\" do not touch this");
+		System.out.println("Tahdah!");
+	}
+}
