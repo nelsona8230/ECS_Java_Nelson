ff2414fbdda7e026e079b0b831aee04061d6edb2
nelsona8230 <NelsonA8230@sduhsd.lan>
2017-11-30 11:11:27 -0800

    From project Using Variables

---
+++ UsingVariables/src/UseVar.java
@@ -0,0 +1,19 @@
+//this is the Using Variable project in which you pu variables after lines of code
+public class UseVar 
+{
+	public static void main(String[] args)
+	{
+		int room;
+		double oroom;
+		String Compsci;
+		
+		room = 113;
+		oroom = 2.71828;
+		Compsci = "Computer Science";
+		
+		System.out.println("This is Room # " + room);
+		System.out.println("e is close to " + oroom);
+		System.out.println("I am learning about " + Compsci );
+	}
+
+}
